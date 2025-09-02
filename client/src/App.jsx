e5da4ee0658140759d2ed3a1de7c0757f4fb9928
Pradhamlr <pradham.lr@gmail.com>
2025-09-02 07:57:46 +0530

    Navbar Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import BlogTitles from './pages/BlogTitles'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from './pages/RemoveBackground'
+import RemoveObject from './pages/RemoveObject'
+import ReviewResume from './pages/ReviewResume'
+import WriteArticle from './pages/WriteArticle'
+import Layout from './pages/Layout'
+import Community from './pages/Community'
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>App</div>
+    <div>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/ai' element={<Layout />} >
+                <Route index element={<Dashboard />} />
+                <Route path='blog-titles' element={<BlogTitles />} />
+                <Route path='generate-images' element={<GenerateImages />} />
+                <Route path='remove-background' element={<RemoveBackground />} />
+                <Route path='remove-object' element={<RemoveObject />} />
+                <Route path='review-resume' element={<ReviewResume />} />
+                <Route path='write-article' element={<WriteArticle />} />
+                <Route path='community' element={<Community />} />
+            </Route>
+        </Routes>
+    </div>
   )
 }
 
