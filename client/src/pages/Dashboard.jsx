46d2ce1e629a41655df00fb54ee484a783166581
Pradhamlr <pradham.lr@gmail.com>
2025-09-16 22:08:25 +0530

    Ai Section

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { dummyCreationData } from '../assets/assets'
+import { Sparkles } from 'lucide-react'
+import Creationitem from '../components/Creationitem'
 
 const Dashboard = () => {
+
+    const [creations, setCreations] = React.useState([])
+    const getDashboardData=  async () => {
+        setCreations(dummyCreationData)
+    }
+
+    useEffect(()=>{
+        getDashboardData()
+    }, []) 
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className='h-full overflow-y-scroll p-6'>
+      <div className='flex gap-4 flex-wrap'>
+        {/*Total Creations Card */}
+        <div className='flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200'>
+            <div className='text-slate-600'>
+                <p className='text-sm'>Total Creations</p>
+                <h2 className='text-xl font-bold'>{creations.length}</h2>
+            </div>
+            <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'>
+                <Sparkles className='w-5 text-white'/>
+            </div>
+        </div>
+      </div>
+
+    <div className= 'space-y-3'>
+        <p className='mt-6 mb-4'>Recent Creations</p>
+        {
+            creations.map((item) =>
+            <Creationitem key={item.id} item={item} />)
+        } 
+    </div>
+
     </div>
   )
 }
